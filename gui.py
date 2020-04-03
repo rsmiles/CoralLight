fe1dda3911f49dfe87fbb8dbcf24269d74d0a66d
Robert Smiley <yarnoiser@gmail.com>
2020-04-03 15:44:35 -0600

    Moved chart generation controls to main window

--- gui.py
+++ gui.py
@@ -8,32 +8,6 @@ import os
 def dataMapNames(datamap):
 	return [datamap[key]['name'] for key in datamap.keys()]
 
-class ChartWindow:
-	def __init__(self, parent):
-		self.parent = parent
-		self.window = tk.Toplevel(self.parent.root)
-		self.title = APP_NAME + ' Chart Creator'
-		self.window.title(self.title)
-		self.initUI()
-
-	def initUI(self):
-		self.chartTypeLabel = tk.Label(self.window, text='Chart Type')
-		self.chartTypeLabel.pack()
-
-		self.chartTypeBox = ttk.Combobox(self.window, values=('Pie', 'Bar'))
-		self.chartTypeBox.pack()
-
-		self.varLabel = tk.Label(self.window, text='Variable')
-		self.varLabel.pack()
-
-		self.varBox = ttk.Combobox(self.window, values=(dataMapNames(agrra.encounter_datamap)))
-		self.varBox.pack()
-		self.genButton = tk.Button(self.window, text='Generate Chart', command=self.genChart)
-		self.genButton.pack()
-
-	def genChart(self):
-		self.window.destroy()
-
 class MainWindow():
 	def __init__(self):
 		self.root = tk.Tk()
@@ -48,28 +22,48 @@ class MainWindow():
 		self.menuBar.add_cascade(label=APP_NAME, menu=self.coralLightMenu)
 
 		self.dataMenu = tk.Menu(self.menuBar, tearoff=False)
-		self.dataMenu.add_command(label='New Database', command=self.new_database, accelerator='Ctrl+Shift+N')
+		self.dataMenu.add_command(label='New Database', command=self.new_database, accelerator='Ctrl+N')
 		self.dataMenu.add_command(label='Open Database', command=self.open_database, accelerator='Ctrl+O')
 		self.dataMenu.add_command(label='Import Data', command=self.import_data, accelerator='Ctrl+I')
 		self.dataMenu.add_command(label='Show Current Database', command=self.show_current_database, accelerator='Ctrl+D')
 		self.menuBar.add_cascade(label='Data', menu=self.dataMenu)
 
 		self.chartMenu = tk.Menu(self.menuBar, tearoff=False)
-		self.chartMenu.add_command(label='New Chart', command=self.new_chart, accelerator='Ctrl+N')
+		self.chartMenu.add_command(label='Save Chart', command=self.save_chart, accelerator='Ctrl+S')
 		self.menuBar.add_cascade(label='Chart', menu=self.chartMenu)
 
 		# Setup keyboard shortcuts
 		self.root.bind_all('<Control-q>', self.quit)
 
 		self.root.bind('<Control-w>', self.quit)
-		self.root.bind('<Control-Shift-N>', self.new_database)
+		self.root.bind('<Control-n>', self.new_database)
 		self.root.bind('<Control-o>', self.open_database)
 		self.root.bind('<Control-i>', self.import_data)
 		self.root.bind('<Control-d>', self.show_current_database)
 
-		self.root.bind('<Control-n>', self.new_chart)
-
 		self.root.config(menu=self.menuBar)
+
+		# Setup chart generation controls
+
+		self.displayLabel = tk.Label(self.root, width=50, height=25)
+		self.displayLabel.pack(side='left')
+
+		self.controlFrame = tk.Frame(self.root)
+		self.controlFrame.pack(side='top')
+
+		self.chartTypeLabel = tk.Label(self.controlFrame, text='Chart Type')
+		self.chartTypeLabel.pack()
+
+		self.chartTypeBox = ttk.Combobox(self.controlFrame, values=('Pie', 'Bar'))
+		self.chartTypeBox.pack()
+
+		self.varLabel = tk.Label(self.controlFrame, text='Variable')
+		self.varLabel.pack()
+
+		self.varBox = ttk.Combobox(self.controlFrame, values=(dataMapNames(agrra.encounter_datamap)))
+		self.varBox.pack()
+		self.genButton = tk.Button(self.controlFrame, text='Generate Chart', command=self.gen_chart)
+		self.genButton.pack()
 
 	def exec_str(self, string):
 		try:
@@ -104,9 +98,12 @@ class MainWindow():
 	def show_current_database(self, event=None):
 		tk_messagebox.showinfo(title='Current Database', message=agrra.config.DB)
 
-	def new_chart(self, event=None):
-		self.chartWindow = ChartWindow(self)
+
+	def save_chart(self, event=None):
+		pass
 
 	def run(self):
 		self.root.mainloop()
 
+	def gen_chart(self):
+		pass
