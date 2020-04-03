7a87ea1d42c3dcdc737492ff4008769d0d6afc68
Robert Smiley <yarnoiser@gmail.com>
2020-04-03 13:01:00 -0600

    Error message popups, remove glade files from .gitignore

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 from app_info import *
 from libcorallight import *
 import tkinter as tk
+import tkinter.messagebox as tk_messagebox
 import os
 
 class MainWindow():
@@ -23,6 +24,12 @@ class MainWindow():
 
 		self.root.config(menu=self.menuBar)
 
+	def exec_str(self, string):
+		try:
+			exec_str(string)
+		except Exception as e:
+			tk_messagebox.showerror('Error', str(e))
+
 	def new_database(self):
 		fileName = tk.filedialog.asksaveasfilename()
 		if not fileName:
@@ -30,19 +37,19 @@ class MainWindow():
 
 		elif os.path.isfile(fileName):
 			os.remove(filename)
-		exec_str('@opendb {0};'.format(fileName))
+		self.exec_str('@opendb {0};'.format(fileName))
 
 	def open_database(self):
 		fileName = tk.filedialog.askopenfilename()
 		if not fileName:
 			return
 
-		exec_str('@opendb {0};'.format(fileName))
+		self.exec_str('@opendb {0};'.format(fileName))
 
 	def import_data(self):
 		fileNames = tk.filedialog.askopenfilenames()
 		for fileName in fileNames:
-			exec_str('@import {0};'.format(fileName))
+			self.exec_str('@import {0};'.format(fileName))
 
 	def run(self):
 		self.root.mainloop()
