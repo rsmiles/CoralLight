9d7450cee6be0f329f1f36c579959619f4026258
Robert Smiley <yarnoiser@gmail.com>
2020-04-23 13:41:29 -0600

    Data generator working

--- datagen.py
+++ datagen.py
@@ -6,14 +6,15 @@ from time import localtime, strftime
 from openpyxl import Workbook
 from openpyxl import load_workbook
 
-assert len(sys.argv) == 2, 'No data file specified'
+assert len(sys.argv) == 3, 'usage: ./datagen.py site_name output_file'
 
 DATAMAP_PATH = config.APP_PATH + 'datamap/'
 SHEET_DATAMAP = DATAMAP_PATH + 'sheet.csv'
 TRANSECT_DATAMAP = DATAMAP_PATH + 'transect.csv'
 ENCOUNTER_DATAMAP = DATAMAP_PATH + 'encounter.csv'
 CORAL_INIT = config.APP_PATH + 'coral.csv'
-DATAFILE = sys.argv[1]
+SITE_NAME = sys.argv[1]
+DATAFILE = sys.argv[2]
 NUM_SHEETS = 2
 
 def isSheetEmpty(ws):
@@ -21,6 +22,9 @@ def isSheetEmpty(ws):
 
 def writeField(ws, dataMap, key, value):
 	ws[dataMap[key]['pos']] = value
+
+def writeRowField(row, dataMap, key, value):
+	row[dataMap[key]['pos']].value = value
 
 def createSheet(wb, title):
 	global sheet_datamap
@@ -40,7 +44,7 @@ def createSheet(wb, title):
 					'revision': '2016-05-27',
 					'surveyor': 'Test Surveyor',
 					'AGRRA_code': 'N/A',
-					'site_name': 'Test Site',
+					'site_name': SITE_NAME,
 					'date': datetime.today().strftime('%d/%m/%Y'),
 					'bottom_temp': 28,
 					'bottom_temp_units': 'ºC',
@@ -64,8 +68,8 @@ def createTransectTable(num):
 					'transect_comments': ''}
 	return transectTable
 
-def createEncounterTable(coralMap):
-	coral = random.choice(coral)
+def createEncounterTable(coralList):
+	coral = random.choice(coralList)
 	species_code = None
 
 	if coral['species'] == '':
@@ -76,7 +80,7 @@ def createEncounterTable(coralMap):
 	length = random.randint(2, 400)
 
 	part_mort_old = random.randint(0, 100)
-	part_mort_trans = random.randint(0, part_mort_old - 1)
+	part_mort_trans = random.randint(0, 100 - part_mort_old)
 
 	encounterTable = {'species_code': species_code,
 						'num_isolates': str(random.randint(1, 11)),
@@ -85,14 +89,14 @@ def createEncounterTable(coralMap):
 						'height': str(random.randint(1, 400)),
 						'percent_pale': '',
 						'percent_bleached':'',
-						'bleach_code': random.choice(('', 'P', 'PB', 'BL'),
-						'part_mort_new': str(part_mort_new),
+						'bleach_code': random.choice(('', 'P', 'PB', 'BL')),
+						'part_mort_old': str(part_mort_old),
 						'part_mort_trans': str(part_mort_trans),
-						'part_mort_new': str(random.randint(0, part_mort_old + part_mort_trans - 1)),
+						'part_mort_new': str(random.randint(0, 100 - part_mort_old - part_mort_trans)),
 						'disease': random.choice(('', 'SCTLD', 'DS', 'YB', 'WB')),
 						'extra_bleach': '',
 						'extra_mort': '',
-						'comments': random.choice(('', 'SCOR', 'TSOL', 'Orange Icing Sponge', 'PCAR', 'HCAR', 'PFB', 'DFB', 'Outplanted'))
+						'comments': random.choice(('', 'SCOR', 'TSOL', 'Orange Icing Sponge', 'PCAR', 'HCAR', 'PFB', 'DFB', 'Outplanted')),
 						'point_count_l' :'',
 						'point_count_p' :'',
 						'point_count_bl': '',
@@ -100,9 +104,20 @@ def createEncounterTable(coralMap):
 						'point_count_om': '',
 						'point_count_other': '',
 						'point_count_interval': ''}
+	return encounterTable
 
-def createTransect(ws, num, numEncounters):
+def createTransect(ws, num, start_row, numEncounters, coralList):
+	global MAX_COL
 	transectTable = createTransectTable(num)
+
+	for row in ws.iter_rows(min_row = start_row, max_col = MAX_COL, max_row=start_row + numEncounters):
+		encounterTable = createEncounterTable(coralList)
+
+		for key in transectTable:
+			writeRowField(row, transect_datamap, key, transectTable[key])
+
+		for key in encounterTable:
+			writeRowField(row, encounter_datamap, key, encounterTable[key])
 	
 
 sheet_datamap = {}
@@ -113,8 +128,8 @@ with open(SHEET_DATAMAP) as mapfile:
 		if row['key'] == 'min_row' or row['key'] == 'max_col':
 			sheet_datamap[row['key']]['pos'] = int(sheet_datamap[row['key']]['pos']) # convert positional values read to integers
 
-MINROW = sheet_datamap['min_row']['pos']
-MAXROW = sheet_datamap['max_col']['pos']
+MIN_ROW = sheet_datamap['min_row']['pos']
+MAX_COL = sheet_datamap['max_col']['pos']
 
 transect_datamap = {}
 with open(TRANSECT_DATAMAP) as mapfile:
@@ -150,8 +165,9 @@ while sheetNum <= NUM_SHEETS:
 
 	ws = createSheet(wb, sheetTitle)
 
-	createTransect(ws, 200)
+	createTransect(ws, transectNum, MIN_ROW, 200, coralList)
 	transectNum += 1
+	createTransect(ws, transectNum, MIN_ROW + 201, 200, coralList)
 	transectNum += 1
 	sheetNum += 1
 
