f29c710bf687587728cb46e84803736399b1cc2d
Robert Smiley <yarnoiser@gmail.com>
2020-04-24 16:38:29 -0600

    Allow for inclusion of data files

--- setup.py
+++ setup.py
@@ -12,12 +12,13 @@ setuptools.setup(
 	description='Coral Data Visualization Tool',
 	long_description=longDescription,
 	long_description_content_type='text/markdown',
-	url='https://github.com/rsmiles/CoralLight'
+	url='https://github.com/rsmiles/CoralLight',
 	classifiers=[
 		'Programming Language :: Python :: 3',
 		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 		'Operating System :: OS Independent',
 	],
-	python3_requires='>=3.6'
+	python3_requires='>=3.6',
+	include_package_data=True,
 )
 
