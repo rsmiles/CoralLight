ec50c8e294a0980286ca622d7915bf2bc9771443
Robert Smiley <yarnoiser@gmail.com>
2020-06-23 00:35:36 -0600

    Start of csvimport.py

---
+++ coral_light/csvimport.py
@@ -0,0 +1,18 @@
+import csv, os, sqlite3
+
+def scanTypes(csv):
+	pass
+
+class DB:
+	def __init__(self, name):
+		pass
+
+	def close(self):
+		pass
+
+	def createTable(self, tableName, typeList):
+		pass
+
+	def importCSV(self, tableName, fileName):
+		pass
+
