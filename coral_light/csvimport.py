9fd7b6e737a82b9e4fe147efe8c2cbe1472f531b
Robert Smiley <yarnoiser@gmail.com>
2020-06-23 23:46:10 -0600

    ScanTypes function

--- coral_light/csvimport.py
+++ coral_light/csvimport.py
@@ -1,7 +1,33 @@
-import csv, os, sqlite3
+import csv, os, re, sqlite3
 
-def scanTypes(csv):
-	pass
+def scanTypes(fileName):
+	types = {}
+	with open(fileName) as inputFile:
+		reader = csv.DictReader(inputFile)
+		for row in reader:
+			for key, value in enumerate(row):
+				if key not in types:
+					types[key] = []
+				if re.match(r'[0-9]+\.[0-9]+', value):
+					if 'REAL' not in types[key]:
+						types[key].append('REAL')
+				elif re.match(r'[0-9]+', value):
+					if 'INTEGER' not in types[key]:
+						types[key].append('INTEGER')
+				else:
+					if 'TEXT' not in types[key]:
+						types[key].append('TEXT')
+	result = []
+
+	for field, typeList in enumerate(types):
+		if 'TEXT' in typeList:
+			result.append((field, 'TEXT'))
+		elif 'REAL' in typeList:
+			result.append((field, 'REAL'))
+		else:
+			result.append((field, 'INTEGER'))
+
+	return result
 
 class DB:
 	def __init__(self, name):
